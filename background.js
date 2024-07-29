3d8f8456b1830d1006bdb7eb3b8bcbf1580fc4a5
Chris Read <rcreadii@gmail.com>
2024-07-29 16:59:56 -0500

    update to username agnostic

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
+// Avoid newsfeeds on: linkedin, twitter
 const redirects = {
     "https://www.linkedin.com/": "https://www.linkedin.com/messaging/thread/new",
     "https://www.linkedin.com/feed/": "https://www.linkedin.com/messaging/thread/new",
-    "https://x.com/": "https://www.x.com/chrisreadsalot6/following",
-    "https://x.com/home": "https://www.x.com/chrisreadsalot6/following",
-    "https://twitter.com/": "https://www.x.com/chrisreadsalot6/following",
-    "https://twitter.com/home": "https://www.x.com/chrisreadsalot6/following"
+    "https://x.com/": "https://www.x.com/messages",
+    "https://x.com/home": "https://www.x.com/messages",
 };
 
 chrome.webNavigation.onBeforeNavigate.addListener(
